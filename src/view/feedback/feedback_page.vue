c6ce05138477dea851608310212d39362b467a16
rensf <2636618553@qq.com>
2019-02-20 15:45:23 +0800

    first blood

---
+++ src/view/feedback/feedback_page.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Card>
+            <div slot="title">
+                <p>
+                    <Icon type="md-remove"/>
+                    问题反馈
+                </p>
+            </div>
+            <div>
+                <Card></Card>
+            </div>
+        </Card>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="less" scoped>
+
+</style>
