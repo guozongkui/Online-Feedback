c93348c3101ad04580bf98764c0bae2d693242bf
rensf <2636618553@qq.com>
2019-02-21 17:32:06 +0800

    添加mysql

---
+++ src/server/db.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    mysql: {
+        host: 'localhost',  // 新建数据库连接时的 主机名或ID地址 内容
+        user: '', //用户名
+        password: '', // 密码
+        database: '', // 数据库名
+        port: '3306'
+    }
+}
