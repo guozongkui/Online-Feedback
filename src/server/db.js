8e060fc6a290952508818f63883e585f8a47a076
rensf <2636618553@qq.com>
2019-02-26 23:31:08 +0800

    连接数据库

--- src/server/db.js
+++ src/server/db.js
@@ -1,9 +1,9 @@
-module.exports = { 
-    mysql: {
-        host: 'localhost',  // 新建数据库连接时的 主机名或ID地址 内容
-        user: '', //用户名
-        password: '', // 密码
-        database: '', // 数据库名
-        port: '3306'
-    }
+module.exports = {
+  mysql: {
+    host: 'localhost', // 新建数据库连接时的 主机名或ID地址 内容
+    user: 'root', // 用户名
+    password: '263661', // 密码
+    database: 'feedback', // 数据库名
+    port: '3306'
+  }
 }
